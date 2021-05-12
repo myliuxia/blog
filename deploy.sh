b44d4b6e7ec969b74974eade8687ed63cbaeeccd
liuxia <1285370080@qq.com>
2021-05-12 10:52:14 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,19 @@
 set -e
 
 # 生成静态文件
-npm run blog:build
+npm run build
 
 # 进入生成的文件夹
-cd blog/.vuepress/dist
+cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-# echo '80shuo.com' > CNAME
+# echo 'xxx.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:myliuxia/myliuxia.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:80maker/80shuo.git master:gh-pages
+git push -f https://github.com/myliuxia/myliuxia.github.io.git master
 
 cd -
